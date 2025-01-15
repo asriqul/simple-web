eec0f0f3149df84cbaeba3a4ea471f906ac8fe5f
asriqul <didipbnc11@gmail.com>
2025-01-15 18:16:59 +0700

    test push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>bakso kontol bakso kontoll</h1>
+      <p>HHAAAAAAAHHHHHHHHAAAAAAAAAAAHHHHHAAAAAAAAA</p>
+      <p>yg baca monyet</p>
+    </>
+  )
+}
+
+export default App
